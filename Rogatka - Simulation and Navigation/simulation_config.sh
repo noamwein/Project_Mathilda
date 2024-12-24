d28a7db23b519b01985377533eb5a5380e7528a0
liammohr45 <liam.mohr.45@gmail.com>
2024-12-24 23:39:19 +0200

    More fixes to installation process:(

--- Rogatka - Simulation and Navigation/simulation_config.sh
+++ Rogatka - Simulation and Navigation/simulation_config.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd ardupilot
+git submodule init
+git submodule update --recursive
+./Tools/gittools/submodule-sync.sh
 ./waf configure --board sitl
 ./waf copter
--- Rogatka - Simulation and Navigation/simulation_installation.sh
+++ Rogatka - Simulation and Navigation/simulation_installation.sh
@@ -3,4 +3,3 @@ sudo apt update
 sudo apt upgrade
 git clone https://github.com/ArduPilot/ardupilot.git
 ardupilot/Tools/environment_install/install-prereqs-ubuntu.sh -y
-. ~/.profile
