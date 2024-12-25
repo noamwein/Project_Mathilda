800902c60271054da32ba04433258b05868f11c4
TamirShahar <56883090+TamirShahar@users.noreply.github.com>
2024-12-25 16:17:31 +0200

    a

--- EagleEye - Image Processing/VideoSource.py
+++ EagleEye - Image Processing/VideoSource.py
@@ -1,6 +1,22 @@
 from abc import ABC, abstractmethod
+from interfaces import Source
+from ImageProcessingConstants import *
+import cv2
 
-class Source(ABC):
-    @abstractmethod
+
+class VideoSource(Source):
+    def __init__(self, video_path: str, start_time: int = 0):
+        self.capture = cv2.VideoCapture(video_path)
+        self.capture.set(cv2.CAP_PROP_POS_MSEC, start_time * 1000)
+        # print("width:", self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # print("height:", self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # print("fps:", self.capture.get(cv2.CAP_PROP_FPS))
+
     def get_current_frame(self):
-        pass
+        ret, frame = self.capture.read()
+        if not ret:
+            raise ValueError("Failed to capture frame from video source.")
+        return frame
+
+    def release(self):
+        self.capture.release()
