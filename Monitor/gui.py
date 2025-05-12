1a54733344b48e92e90cfbc4fefa72f6e810d6b8
Ariel Ben Eliezer <ariel.ben.eliezer.45@gmail.com>
2025-05-12 21:11:03 +0300

    added gui and dummy client

--- Monitor/gui.py
+++ Monitor/gui.py
@@ -1,16 +1,26 @@
+import cv2
+
+from EagleEye.image_detection_models.color_detection_model import CENTERED_X, CENTERED_Y, ImageDetection
+from EagleEye.image_detection_models.color_detection_model import ColorImageDetectionModel
+from EagleEye.sources.camera_source import CameraSource
 from Monitor.video_saver import VideoSaver
 from Rogatka.drone_client import DroneClient
-from EagleEye.image_detection_models.color_detection_model import CENTERED_X, CENTERED_Y, ImageDetection
-
-import cv2
+from Rogatka.dummy_client import DummyClient
 
 
 class GUI:
-    def __init__(self, drone_client: DroneClient, video_saver: VideoSaver, image_detection: ImageDetection, enable_display=True):
+    def __init__(self, drone_client: DroneClient, video_saver: VideoSaver, image_detection: ImageDetection,
+                 enable_display=True):
         self.drone_client = drone_client
         self.video_saver = video_saver
         self.enable_display = enable_display
         self.image_detection = image_detection
+
+        # Create a named window
+        cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+
+        # Set the window to fullscreen
+        cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     def draw_gui(self, frame):
         if frame is None:
@@ -20,8 +30,6 @@ class GUI:
             self.video_saver.write_frame(processed_frame)
         if self.enable_display:
             cv2.imshow('Video', processed_frame)
-            cv2.waitKey(1)
-
 
     def _draw_gui(self, frame):
         bbox = self.image_detection.image_detection_data.get('bbox')
@@ -44,3 +52,15 @@ class GUI:
         y_circle = (y_min + y_max) // 2
         cv2.circle(frame, (x_circle, y_circle), 5, (255, 0, 0), -1)
         cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 3)
+
+
+def main():
+    source = CameraSource()
+    gui = GUI(drone_client=DummyClient(), video_saver=VideoSaver(), image_detection=ColorImageDetectionModel(None))
+    while True:
+        frame = source.get_current_frame()
+        gui.draw_gui(frame)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Rogatka/dummy_client.py
@@ -0,0 +1,82 @@
+import random
+
+from BirdBrain.interfaces import DroneClient
+from typing import List, Tuple
+from BirdBrain.interfaces import Waypoint, Source, ImageDetection
+
+
+class DummyClient(DroneClient):
+    def set_speed_for_duration(self, velocity_x: float, velocity_y: float, velocity_z: float, duration_seconds: int):
+        pass
+
+    def set_speed(self, velocity_x: float, velocity_y: float, velocity_z: float):
+        pass
+
+    def check_if_mode_guided(self) -> bool:
+        return True
+
+    def connect(self):
+        pass
+
+    def get_current_location(self):
+        pass
+
+    def is_armed(self):
+        return True
+
+    def takeoff(self):
+        pass
+
+    def get_altitude(self):
+        return 10 + random.random() * 5
+
+    def get_heading(self):
+        pass
+
+    def follow_path(self, waypoints: List[Waypoint], source_obj: Source, detection_obj: ImageDetection):
+        pass
+
+    def get_initial_altitude(self):
+        return 5
+
+    def pid(self, target_position):
+        pass
+
+    def has_stopped(self):
+        return False
+
+    def stop_movement(self):
+        pass
+
+    def is_on_target(self, target_position: Tuple[int, int]):
+        pass
+
+    def distance_from_home(self) -> float:
+        pass
+
+    def return_to_launch(self):
+        pass
+
+    def land(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def move_forward(self, distance):
+        pass
+
+    def log_and_print(self, message: str):
+        pass
+
+    def rotate(self, angle):
+        pass
+
+    def change_altitude(self, delta):
+        pass
+
+    def mission_completed(self):
+        pass
+
+    def assassinate(self):
+        pass
