8d4c5e7ac6a7b031769882f334c170587fb2f3b2
Liam Mohr <liam.mohr.45@gmail.com>
2025-05-15 02:17:57 +0300

    fixed imports

--- Monitor/main.py
+++ Monitor/main.py
@@ -1,7 +1,7 @@
 import sys, os
 
+# allow importing from parent directory
 sys.path.append(os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir)))
-sys.path.append(os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir,os.pathdir)))
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel
 from PySide6.QtCore import Qt, QTimer
