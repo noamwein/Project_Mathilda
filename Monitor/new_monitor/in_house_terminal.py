010404abbd2bc8c5e1cb7c27c0ce8d340949db44
Ariel Ben Eliezer <ariel.ben.eliezer.45@gmail.com>
2025-05-15 05:51:58 +0300

    changed font

--- Monitor/new_monitor/in_house_terminal.py
+++ Monitor/new_monitor/in_house_terminal.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QTextEdit, QVBoxLayout
 class InHouseTerminal(MonitorPanel):
     def setup_ui(self):
         self.text_edit = QTextEdit()
+        self.text_edit.setStyleSheet("font-family: 'DejaVu Sans Mono'; font-size: 12px;")
         self.text_edit.setReadOnly(True)
         layout = QVBoxLayout(self)
         layout.addWidget(self.text_edit)
