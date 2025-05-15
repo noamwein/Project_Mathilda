1faea9797c8486a091235659d69d1056d4e6c61d
Ariel Ben Eliezer <ariel.ben.eliezer.45@gmail.com>
2025-05-15 05:58:54 +0300

    not readonly

--- Monitor/new_monitor/in_house_terminal.py
+++ Monitor/new_monitor/in_house_terminal.py
@@ -5,7 +5,7 @@ class InHouseTerminal(MonitorPanel):
     def setup_ui(self):
         self.text_edit = QTextEdit()
         self.text_edit.setStyleSheet("font-family: 'DejaVu Sans Mono'; font-size: 12px;")
-        self.text_edit.setReadOnly(True)
+        # self.text_edit.setReadOnly(True)
         layout = QVBoxLayout(self)
         layout.addWidget(self.text_edit)
 
